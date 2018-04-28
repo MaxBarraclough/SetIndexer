7118a420b5f968990ef8ed85163df6ba8ddecb5b
Max Barraclough <maxbarraclough@users.noreply.github.com>
2018-04-28 19:01:37 +0100

    Initial commit
    Have specified GPLv3, have things in place
    for a CLI application with Maven

---
+++ src/main/java/engineer/maxbarraclough/setindexer_CLI/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Licence: see LICENCE.txt
+ */
+package engineer.maxbarraclough.setindexer_CLI;
+
+/**
+ *
+ * @author mb
+ */
+public final class Main {
+
+    public static void main(String[] args)
+    {
+        // return 0;
+    }
+}
